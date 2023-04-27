641899b08f4ea2684b82d032f934cab02901e194
Dym6lite <78ah5v7@code.edu.az>
2023-04-27 02:43:02 -0700

    hometask

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const arr = [];
+const input = prompt();
+
+const num2Str = input.toString();
+const count = num2Str.length;
+
+alert("Arraya elave edilmish " + num2Str + "-in element(ler)inin sayi: " + count);
